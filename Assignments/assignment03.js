79e753f57743962bdf07867f633cacb9331c182b
PurplePenguin414 <89933904+PurplePenguin414@users.noreply.github.com>
2021-10-17 11:58:44 -0400

    Update assignment03.js

--- Assignments/assignment03.js
+++ Assignments/assignment03.js
@@ -27,3 +27,20 @@ function add1 (elem) {
   }
 }
 
+for(let i=1; i<=18; i++) {
+  // console.log(i);
+  elem[i] = document.getElementById(i.toString());
+  elem[i].children[4].children[1].onclick = function(){down1(elem[i]);};
+}
+
+// create an "add1" function
+function down1 (elem) {
+  if(elem.children[2].innerHTML == "+") 
+    elem.children[2].innerHTML = "1";
+  else {
+    let currentScore = elem.children[2].innerHTML;
+    currentScore = Number.parseInt(currentScore);
+    elem.children[2].innerHTML = currentScore - 1;
+  }
+}
+
