910bd6e765b4539e426b2f0b00a703b5ba7fdb96
PurplePenguin414 <89933904+PurplePenguin414@users.noreply.github.com>
2021-10-25 00:04:35 -0400

    Update clubMeNow.js

--- Assignments/Assignment04/clubMeNow.js
+++ Assignments/Assignment04/clubMeNow.js
@@ -128,7 +128,7 @@ function resetAllClubDistances() {
 // navigate to "About" screen
 function displayAbout() {
 	// your code here
-	alert("Hello friends, this is message.");
+	alert("ClubMeNow version 1.0.0");
 	// window.location.href = "clubAbout.html";
 	window.location.href = "clubAbout.html";
 }
