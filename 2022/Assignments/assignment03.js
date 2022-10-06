fd95aba442d0e846e45ddcb1b2e1a2528960bdc1
PurplePenguin414 <89933904+PurplePenguin414@users.noreply.github.com>
2022-10-06 10:05:06 -0400

    Update assignment03.js

--- 2022/Assignments/assignment03.js
+++ 2022/Assignments/assignment03.js
@@ -11,3 +11,16 @@ function GCD(x, y) {
   document.getElementById("out").innerHTML= x;
   return x;
 }
+
+function sumDigits() {
+    var value = document.getElementById("n").value,
+        sum = 0;
+
+  while (value) {
+      sum += value % 10;
+      value = Math.floor(value / 10);
+  }
+  
+  var rezultat = document.getElementById("result");
+  rezultat.value = sum;
+}
