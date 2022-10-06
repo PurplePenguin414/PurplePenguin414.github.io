6a458d7f84d3f5279eed680b5c8d1969ba3b7bda
PurplePenguin414 <89933904+PurplePenguin414@users.noreply.github.com>
2022-10-06 09:44:58 -0400

    Update assignment03.js

--- 2022/Assignments/assignment03.js
+++ 2022/Assignments/assignment03.js
@@ -1,5 +1,13 @@
-
-function convertF2C(f,c) {
-  f = parseFloat(f);
-  document.getElementById("c").innerHTML=(f-32)/1.8;
+function GCD(x, y) {
+  if ((typeof x !== 'number') || (typeof y !== 'number')) 
+    return false;
+  x = Math.abs(x);
+  y = Math.abs(y);
+  while(y) {
+    var t = y;
+    y = x % y;
+    x = t;
+  }
+  document.getElementById("out").innerHTML= x;
+  return x;
 }
