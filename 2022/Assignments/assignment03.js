d6f90d3e3b5e6dedefd482504e49a54fb73c1d5a
PurplePenguin414 <89933904+PurplePenguin414@users.noreply.github.com>
2022-10-13 09:53:46 -0400

    Update assignment03.js

--- 2022/Assignments/assignment03.js
+++ 2022/Assignments/assignment03.js
@@ -1,17 +1,27 @@
-function GCD(x, y) {
-  x = Math.abs(x);
-  y = Math.abs(y);
-  while(y) {
-    let t = y;
-    y = x % y;
-    x = t;
-  }
-  let result = document.getElementById("03B");
-  result.value = x;
-}
+function GCD() {
 
+    let x = parseInt(document.getElementById("one").value);
+    let y = parseInt(document.getElementById("two").value);
+
+    if(typeof(x) != "number" || typeof(y) != "number") {
+        document.getElementById("03B").value = "Please enter two numbers";
+        return;
+    }
+        
+    /* This snippet is borrowed from "w3resource.com" */
+    x = Math.abs(x);
+    y = Math.abs(y);
+
+    while(y) {
+        let t = y;
+        y = x % y;
+        x = t;
+        }
+    /* end of borrowed snippet  */
+    document.getElementById("03B").value = x;
+}
 function sumDigits() {
-    let value = document.getElementById("n").value,
+    var value = document.getElementById("n").value,
         sum = 0;
 
   while (value) {
@@ -19,19 +29,27 @@ function sumDigits() {
       value = Math.floor(value / 10);
   }
   
-  let result = document.getElementById("result");
-  result.value = sum;
+  var rezultat = document.getElementById("03C");
+  rezultat.value = sum;
 }
-
 function fullName() {
-  let title = document.getElementById("title").value,
-  fname = document.getElementById("fname").value,
-  lname = document.getElementById("lname").value,
-  s = document.getElementById("s").value,
-  Fullname = `${title}. ${fname} ${lname}, ${s}`;
-  
-  let name = document.getElementById("03D");
-  name.value = Fullname;
+    let s = document.getElementById("s").value;
+    let fname = document.getElementById("fname").value;
+    let lname = document.getElementById("lname").value;
+    let title = document.getElementById("title").value;
+    
+    if(s == "none")
+        s = "";
+
+    if(fname.length > 0)
+        fname += " ";
+    if(lname.length > 0)
+        lname += " ";
+    if(title == "none")
+        title = "";
+    else title += " ";
+
+    document.getElementById("03D").value = title + fname + lname + s;
 }
 
 var tb1 = document.getElementById('tic');
