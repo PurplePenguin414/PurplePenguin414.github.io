749b2d0e56a3973e3117ae9ca9ccd2c12444c882
MGibbs414 <meganleann413@gmail.com>
2023-04-17 19:06:15 -0400

    help

---
+++ 2023/CS216/Assignmnents/GameProject/Attempt2/OtherLocation.java
@@ -0,0 +1,8 @@
+package Attempt2;
+
+/**
+ * Places where a visible card can be located, 
+ * except for the foundation piles and the tableau.
+ */
+public enum OtherLocation implements Location
+{ DISCARD_PILE  }
